cb6cd08a010765500ee3a360885fa6f3dac7eb7b
Nester44 <nesterov.mail4@gmail.com>
2023-11-14 21:24:35 +0100

    enhancement: hide active list on empty field

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,7 @@ import ModePicker from './components/modePicker/modePicker'
 import { useField } from './hooks/useField'
 
 export default function App() {
-	const [fieldSize, setFieldSize] = useState(3)
+	const [fieldSize, setFieldSize] = useState(0)
 	const { field, toggleCell, activeCells } = useField(fieldSize)
 
 	return (
@@ -13,7 +13,7 @@ export default function App() {
 			<ModePicker setFieldSize={setFieldSize} />
 			<div className='flex gap-4 mt-4'>
 				<Grid field={field} toggleCell={toggleCell} />
-				<ActiveList activeCells={activeCells} />
+				{fieldSize > 0 && <ActiveList activeCells={activeCells} />}
 			</div>
 		</div>
 	)
