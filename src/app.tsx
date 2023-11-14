36cc134922b7de8fba818dba6d7098978c4f6a9e
Nester44 <nesterov.mail4@gmail.com>
2023-11-14 19:55:27 +0100

    feat: implemented grid

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,8 @@ import Grid from './components/grid'
 import { useField } from './hooks/useField'
 
 export default function App() {
-	const { field, toggleCell, activeCells } = useField(10)
+	const { field, toggleCell } = useField(10)
 
-	console.log(activeCells)
 	return (
 		<div className='flex flex-col items-center'>
 			<h1>Hello, world!</h1>
